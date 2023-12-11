b47aa41dab78b27c46e2ef333b60c818634a5fdf
skamp931 <43207451+skamp931@users.noreply.github.com>
2023-12-11 21:02:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,38 +7,19 @@ import matplotlib.pyplot as plt
 import japanize_matplotlib
 
 input_num = st.number_input("input_a_number",value=0)
-
 result =input_num ** 2
 st.write("Result",result)
 
 st.title('streamlit Tutorial')
 
-st.header('This is a header')
-
-st.subheader('This is a subheader')
-
-st.text('Hello World!')
-
-st.write('# headline1')
-
-st.markdown('# headline2')
-
-st.write(['apple', 'orange', 'banana'])
-
 df = pd.DataFrame({
     'name': ['Alice', 'Bob'],
     'age': [25, 30],
     'gender': ['female', 'male']
-})
+},index=df["name"])
 
 # DataFrameを表示
 st.write(df)
-# st.dataframe()でも表示可能
-st.dataframe(df)
-
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    st.write(uploaded_file)
 
 if st.button('Say hello'):
     st.write('Hello World!')
